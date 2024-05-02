7662c769c4dfefc3b60b1100c35ee40390fb5416
eprager <eprager@sees.ai>
2024-05-02 14:29:42 +0100

    yaw uncertainty tweak

--- AirLib/include/sensors/gps/GpsSimple.hpp
+++ AirLib/include/sensors/gps/GpsSimple.hpp
@@ -93,7 +93,7 @@ namespace airlib
                 : output.gnss.eph <= params_.eph_min_2d ? GnssFixType::GNSS_FIX_2D_FIX
                                                         : GnssFixType::GNSS_FIX_NO_FIX;
 
-            output.gnss.yaw_uncertainty = output.gnss.fix_type > GnssFixType::GNSS_FIX_2D_FIX ? 0.1 : 7.0;
+            output.gnss.yaw_uncertainty = output.gnss.fix_type > GnssFixType::GNSS_FIX_2D_FIX ? 0.0001 : 0.01;
 
             output.time_stamp = clock()->nowNanos();
 
